0a86026d312c2fd11b82d8c78c4a27cad80a3edd
Armando Ugalde <uarmando15@yahoo.com>
2022-05-29 20:32:07 -0700

    added properties for intern

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const Manager = require("./lib/Manager");
 const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Employee = require("./lib/Employee");
 
 function init() {
     createManager();
@@ -154,4 +154,35 @@ function createCard(employee) {
         `;
     }
 }
+
+function createHTML() {
+    const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title> Our Team Profile</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center"> Our Team Profile</h1>
+                </div>
+            </div>
+            <div class="row">
+                ${employeeArr.map(employee => createCard(employee)).join("")}
+            </div>
+        </div>
+    </body>
+    </html>
+    `;
+    console.log(html);
+}
+
+fs.writeFileSync("/dist/index.html", html);
+
 init();
