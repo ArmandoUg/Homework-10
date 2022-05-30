b7c6dc7e0d8efa0ecf7efb24edb6903e65bf96aa
Armando Ugalde <uarmando15@yahoo.com>
2022-05-30 15:46:36 -0700

    added styling to html page and added text content to readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Homework-10
+# Homework-10 "Team Profile Enginerator"
+
+
+## Description
+This is the readme file for my Team Profile "Enginerator". This script will provide users an easy way to easily create html page with Employee cards that has simple demographics of the employee as well as contact info and links to some employees github account. This app was created with just javascript that is to be used with node. Node js would prompt users to answer a series of questions that would populate the employee cards. Depending on the employee picked, different questions will be asked. Once the user is done with the prompts, an html page is constructed with a polished ui for the user. The html content is created and appended dynamically via java script.
+
+## Here is a screenshot of a html created after the "Enginerator" is used by a user.
+
+
+![Screenshot of created team page]()
+
+
+
+
+## Demo
+Here is a link to a html page that has a short video demo of how a user operates the "Enginerator".
+
+## Links to git hub repo
+- [Github link](https://github.com/ArmandoUg/Homework-10)
--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function addmoreEmployees() {
         } else if (response.addmore === "New Intern") {
             createIntern();
         } else {
-            createHTML();
+            fs.writeFileSync(`dist/index.html`,createHTML());
         }
     });
 }
@@ -76,7 +76,7 @@ function createEngineer() {
         },
         {
             type: "input",
-            message: "What is your engineer's github?",
+            message: "What is your engineer's github name?",
             name: "github"
         }
     ]).then(function (response) {
@@ -118,36 +118,38 @@ function createIntern() {
 function createCard(employee) {
     if (employee.getRole() === "Manager") {
         return `
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 20rem; box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;">
             <div class="card-body">
-                <h5 class="card-title">${employee.getName()}</h5>
+                <h5 class="card-title" style="border-bottom: solid;background-color: lavender;">${employee.getName()}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">${employee.getRole()}</h6>
                 <p class="card-text">ID: ${employee.getId()}</p>
-                <p class="card-text">Email: ${employee.getEmail()}</p>
+                <p class="card-text">Email: <a href = "mailto:${employee.getEmail()}">${employee.getEmail()}</a></p>
                 <p class="card-text">Office Number: ${employee.getOfficeNumber()}</p>
             </div>
         </div>
         `;
     } else if (employee.getRole() === "Engineer") {
         return `
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 20rem; box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;">
             <div class="card-body">
-                <h5 class="card-title">${employee.getName()}</h5>
+                <h5 class="card-title" style="border-bottom: solid;background-color: lightskyblue;">${employee.getName()}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">${employee.getRole()}</h6>
                 <p class="card-text">ID: ${employee.getId()}</p>
-                <p class="card-text">Email: ${employee.getEmail()}</p>
-                <p class="card-text">Github: ${employee.getGithub()}</p>
+                <p class="card-text">Email: <a href = "mailto:${employee.getEmail()}">${employee.getEmail()}</a></p>
+                <p class="card-text">Github: <a href = "https//github.com/${employee.getGithub()}">${employee.getGithub()}</a></p>
             </div>
         </div>
         `;
     } else if (employee.getRole() === "Intern") {
         return `
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 20rem; box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;">
             <div class="card-body">
-                <h5 class="card-title">${employee.getName()}</h5>
+                <h5 class="card-title" style="border-bottom: solid;background-color: lightgreen
+                ;">${employee.getName()}</h5>
+                <h5 class="card-title" >${employee.getName()}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">${employee.getRole()}</h6>
                 <p class="card-text">ID: ${employee.getId()}</p>
-                <p class="card-text">Email: ${employee.getEmail()}</p>
+                <p class="card-text">Email: <a href = "mailto:${employee.getEmail()}">${employee.getEmail()}</a></p>
                 <p class="card-text">School: ${employee.getSchool()}</p>
             </div>
         </div>
@@ -166,23 +168,23 @@ function createHTML() {
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <title> Our Team Profile</title>
     </head>
-    <body>
+    <body style="background-image: ./assets/team.avif">
         <div class="container">
-            <div class="row">
-                <div class="col-12">
+            <div class="row" style="margin-bottom: 4%;">
+                <div class="col-12" style="background-color: lightcoral;">
                     <h1 class="text-center"> Our Team Profile</h1>
                 </div>
             </div>
-            <div class="row">
+            <div class="row" style="justify-content: space-evenly;">
                 ${employeeArr.map(employee => createCard(employee)).join("")}
             </div>
         </div>
     </body>
     </html>
     `;
-    console.log(html);
+    // console.log(html);
+    return html;
 }
 
-fs.writeFileSync("/dist/index.html", html);
 
 init();
